ffd0553b63fa2bd45e7af53ced186ffad49c2d2f
mvldn <mvldn@users.noreply.github.com>
2021-02-12 15:43:54 +0100

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'passGen'`, () => {
+  it(`should have as title 'passwordGenerator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('passGen');
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('passGen app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('passwordGenerator app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passGen';
+  length: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  password: string = '';
+  
+
+  onButtonClick(){
+    const numbers = '1234567890';
+    const letters ='abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%&/()=';
+
+    let validChars = '';
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+    let generatedPasword = '';
+    for(let i=0; i < this.length; i++){
+      const index = Math.floor(Math.random()*validChars.length);
+      generatedPasword += validChars[index];
+    }
+    this.password=generatedPasword;
+  }
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue; 
+    }    
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
 }
